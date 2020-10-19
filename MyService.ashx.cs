2204ff6111c6694f00ea605445a3f012b5c22462
Vasiliy0209 <54793048+Vasiliy0209@users.noreply.github.com>
2020-10-19 15:51:23 +0500

    Add files via upload

---
+++ MyService.ashx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CookComputing.XmlRpc;
+
+namespace MyXmlRpcApplication
+{
+    /// <summary>
+    /// Сводное описание для MyService
+    /// </summary>
+    public class MyService : XmlRpcService
+    {
+        [XmlRpcMethod("myservice::helloworld",Description ="Метод, выводящий приветствие")]
+        public string HelloWorld()
+        {
+            return "Hello, world!";
+        } 
+        
+        [XmlRpcMethod("myservice::cities")]
+        public string Cities(int city_id)
+        {
+            Dictionary<int, string> cities = new Dictionary<int, string>();
+
+            cities.Add(74, "Челябинск");
+            cities.Add(96, "Екатеринбург");
+            cities.Add(77, "Москва");
+
+            if (!cities.ContainsKey(city_id))
+                throw new XmlRpcFaultException(100,"Город с таким кодом не найден");
+
+
+            return cities[city_id];
+        }
+    }
+}
